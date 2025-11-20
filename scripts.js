df1024e7323b1d791a0b34672bfdb155806f186e
Melissa Grant <grantmelissa92@gmail.com>
2025-11-20 03:31:20 -0500

    Addedd updates to landing page

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
-/* Add your JavaScript to this file */
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.querySelector("form");
+    const input = document.querySelector("#email");
+    const message = document.querySelector(".message");
+
+    form.addEventListener("submit", function(event){
+        event.preventDefault();
+
+        const email = input.value.trim();
+        if(email === ""){
+            message.textContent = "Please enter a valid email address.";
+        }else{
+            message.textContent = `Thank you! Your email address ${email} has been added to our mailing list!`;
+        }
+    })
+})
